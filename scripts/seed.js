d21db9633f6e54e8fc520ba2b6273f7e9370aaeb
Himanshu Mishra <himanshumishra@Himanshus-Laptop.local>
2024-12-19 11:11:10 +0530

    Adding postgres DB with seeding in the configuration

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,18 +1,18 @@
-import { query } from "./db.js";
+import { db } from "@vercel/postgres";
 
-const seedDatabase = async () => {
+const seedDatabase = async (client) => {
   try {
     console.log("Seeding the database...");
 
     // Create tables (if not already created)
-    await query(`
+    await client.sql`
       CREATE TABLE IF NOT EXISTS actions (
         id SERIAL PRIMARY KEY,
         pokeId VARCHAR(100) UNIQUE NOT NULL,
         value VARCHAR(255) UNIQUE NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
-    `);
+    `;
 
     console.log("Database has been seeded!");
   } catch (error) {
@@ -22,4 +22,17 @@ const seedDatabase = async () => {
   }
 };
 
-seedDatabase();
+async function main() {
+  const client = await db.connect();
+
+  await seedDatabase(client);
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error(
+    "An error occurred while attempting to seed the database:",
+    err
+  );
+});
