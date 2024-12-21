8059d8a963b2ce61e4bd04f73547482bbb287f06
Himanshu Mishra <himanshumishra@Himanshus-Laptop.local>
2024-12-21 17:48:15 +0530

    Updating car swipe component for pokeswipe

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -1,13 +1,13 @@
-import { TPageProps } from "@/app/interfaces/Global";
+import { PageProps } from "@/.next/types/app/page";
 import { getPokemon } from "@/app/services/getRandomPokemon";
 import Card from "@/app/ui/Card";
 import SwipeableCard from "@/app/ui/Swipeable";
 
-export default async function Match({ params }: TPageProps) {
-  const { id } = params;
+export default async function Match({ params }: PageProps) {
+  const { id } = await params;
   const user = await getPokemon(parseInt(id));
   return (
-    <SwipeableCard params={params}>
+    <SwipeableCard params={{ id }}>
       {(handleSwiper) => <Card user={user} handleSwiper={handleSwiper} />}
     </SwipeableCard>
   );
